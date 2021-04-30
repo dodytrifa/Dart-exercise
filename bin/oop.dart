d52df98b08099d293ed9d5f18908a27a34a3e01b
dodytrifa <dody.trifa@gmail.com>
2021-05-01 03:14:02 +0700

    setup oop

---
+++ bin/oop.dart
@@ -0,0 +1,19 @@
+class Book {
+  String? title;
+  String? author;
+  int? numPages;
+}
+main() {
+  Book madre = Book();
+
+  madre.title = "judul";
+  madre.author = "Joni";
+  madre.numPages = 1000;
+
+  if(type == int madre.numPages ){
+  print("yes");
+
+  }else {
+    print("nope");
+  }
+}
